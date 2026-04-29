17365db969ca1156a2e90123fcc1399e0fb8ed4a
JonatasGdeC <jonatas.07022003@gmail.com>
2026-04-29 16:16:38 -0300

    Ajustes gerais

---
+++ Portifolio/Components/Layout/MainLayout/MainLayout.razor.cs
@@ -0,0 +1,21 @@
+namespace Portifolio.Components.Layout.MainLayout;
+
+public partial class MainLayout
+{
+    private string ThemeClass => UiState.Theme == SiteTheme.Light ? "theme-light" : "theme-dark";
+
+    protected override void OnInitialized()
+    {
+        UiState.Changed += HandleUiChanged;
+    }
+
+    private void HandleUiChanged()
+    {
+        InvokeAsync(workItem: StateHasChanged);
+    }
+
+    public void Dispose()
+    {
+        UiState.Changed -= HandleUiChanged;
+    }
+}
